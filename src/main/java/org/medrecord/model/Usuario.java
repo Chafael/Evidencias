fc053a145699517a8ac7aa9d65175469aa8b35da
peneuvabombon <pene@gmail.com>
2025-07-08 15:17:12 -0600

    Api perrona

---
+++ src/main/java/org/medrecord/model/Usuario.java
@@ -0,0 +1,69 @@
+package org.medrecord.model;
+
+import java.util.Date;
+
+public class Usuario {
+    private int id_usuario;
+    private String nombre;
+    private String apellidos;
+    private Date fechaNacimiento;
+    private String email;
+    private String contrasena;
+    private String genero;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
